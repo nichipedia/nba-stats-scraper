11c767df9578c32763f5a3d5a8312809e06ebecc
Nicholas Moran <atreayou@gmail.com>
2019-02-09 11:15:10 -0600

    renamed stats scraper to be a year to date scraper....

--- nba_ytd_stats_scaper.py
+++ nba_ytd_stats_scaper.py
@@ -18,23 +18,26 @@ def get_team_game_log(teamUrl):
     get_team_stats(game_log_page)
 
 def get_team_stats(teamUrl):
+    ytdStats = []
     raw = simple_get(teamUrl)
     html = BeautifulSoup(raw, 'html.parser')
     table = html.find('table', {'id': 'tgl_basic'})
     for tr in table.tbody.select('tr'):
         if tr.get('id') is not None:
-            #Need to load this into either a dictionary OR a dataframe!
+            #Need to load this into either a dictionary OR a dataframe OR list!
+            dateStats = []
             for td in tr.select('td'):
-                print(td.text)
+                dateStats.append(td.text)
+            print(dateStats)
+            ytdStats.append(dateStats)
 
 
+
+# Main Loop Below
 teamsUrl = baseUrl + '/teams/'
-
 raw = simple_get(teamsUrl)
 html = BeautifulSoup(raw, 'html.parser')
-
 table = html.find('table', {'id': 'teams_active'})
-
 for tr in table.tbody.select('tr'):
     if tr.th.get('class') is not None:
         #print(tr.th['class'])
